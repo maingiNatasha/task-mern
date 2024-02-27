d1394a2b830be2bf62e8ee16f3b0fb4fec02101a
[Natasha Maingi] <mainginatasha@gmail.com>
2024-02-27 12:57:57 +0300

    Added the backend code for the application

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { registerUser, loginUser, getCurrentUser } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.post('/', registerUser)
+router.post('/login', loginUser)
+router.get('/current', protect, getCurrentUser)
+
+module.exports = router;
