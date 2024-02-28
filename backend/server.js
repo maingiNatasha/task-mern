4054d33da88c46960a9460c46120b1ac7f0d6a8d
[Natasha Maingi] <mainginatasha@gmail.com>
2024-02-28 14:49:11 +0300

    Installed cors and implemented it in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const errorHandler = require('./middleware/errorMiddleware');
 const connectDB = require('./connect/database');
 
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
