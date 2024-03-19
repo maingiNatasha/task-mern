dadd5440b4373c88e5a5a4888a334cacd1ee4410
[Natasha Maingi] <mainginatasha@gmail.com>
2024-03-19 14:46:18 +0300

    Fixed some issues related to CORS

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/users/';
+const API_URL = 'https://taskmern-dhbio5wb.b4a.run/api/users/';
 
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData);
--- frontend/src/features/tasks/taskService.js
+++ frontend/src/features/tasks/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = '/api/tasks/';
+const API_URL = 'https://taskmern-dhbio5wb.b4a.run/api/tasks/';
 
 const createTask = async (taskData, token) => {
     const config = {
